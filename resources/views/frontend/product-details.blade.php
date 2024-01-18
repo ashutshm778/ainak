c8b3575657215d792bdffccc363097b311e714af
ashutshm778 <ashutoshm778@gmail.com>
2024-01-18 17:02:26 +0530

    updt

--- resources/views/frontend/product-details.blade.php
+++ resources/views/frontend/product-details.blade.php
@@ -6,13 +6,13 @@
             <div class="row">
                 <div class="col-12">
                      <div class="row ec_breadcrumb_inner">
-                        <div class="col-md-6 col-sm-12">
+                        {{-- <div class="col-md-6 col-sm-12">
                             <h2 class="ec-breadcrumb-title">{{ $data->name }}</h2>
-                        </div>
+                        </div> --}}
                         <div class="col-md-6 col-sm-12">
                             <ul class="ec-breadcrumb-list">
                                 <li class="ec-breadcrumb-item"><a href="{{ route('index') }}">Home</a></li>
-                                <li class="ec-breadcrumb-item active">Products</li>
+                                <li class="ec-breadcrumb-item active">Product Details</li>
                             </ul>
                         </div>
                     </div>
