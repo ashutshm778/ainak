107e5d05f842c42d5983770ee9bfad8f41f38ddc
shaileshhq@gmail.com <shaileshhq@gmail.com>
2024-01-20 16:43:55 +0530

    updte

--- resources/views/frontend/user-history-details.blade.php
+++ resources/views/frontend/user-history-details.blade.php
@@ -1,23 +1,6 @@
 @extends('frontend.layouts.app')
 @section('content')
     <div class="sticky-header-next-sec  ec-breadcrumb section-space-mb">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="row ec_breadcrumb_inner">
-                        <div class="col-md-6 col-sm-12">
-                            <h2 class="ec-breadcrumb-title">User History</h2>
-                        </div>
-                        <div class="col-md-6 col-sm-12">
-                            <ul class="ec-breadcrumb-list">
-                                <li class="ec-breadcrumb-item"><a href="{{route('index')}}">Home</a></li>
-                                <li class="ec-breadcrumb-item active">History</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
     <section class="ec-page-content ec-vendor-uploads ec-user-account section-space-p">
         <div class="container">
