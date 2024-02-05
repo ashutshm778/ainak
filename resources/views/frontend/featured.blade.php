d111182fd2eb75002006df2a059e3cb086c34be5
ashutshm778 <ashutoshm778@gmail.com>
2024-02-05 14:10:46 +0530

    new update

--- resources/views/frontend/featured.blade.php
+++ resources/views/frontend/featured.blade.php
@@ -5,4 +5,7 @@
 @endphp
 @include('frontend.product')
 @endforeach
+@if($features->currentPage()==$features->lastPage())
+@else
 <a style="display:none;" id="current_page_feature" name="example1" class="btn btn-secondary rds {{ ($features->currentPage() == $features->lastPage()) ? ' disabled' : '' }}" href="{{ $features->url($features->currentPage()+1) }}"   >View More <i class="ecicon eci-chevron-right"></i></a>
+@endif
--- resources/views/frontend/new_arrival.blade.php
+++ resources/views/frontend/new_arrival.blade.php
@@ -5,5 +5,8 @@
     @endphp
     @include('frontend.product')
 @endforeach
+@if($new_arriavls->currentPage()==$new_arriavls->lastPage())
+@else
 <a style="display:none;" id="current_page_new_arriavl" name="example" class="btn btn-secondary rds {{ ($new_arriavls->currentPage() == $new_arriavls->lastPage()) ? ' disabled' : '' }}" href="{{ $new_arriavls->url($new_arriavls->currentPage()+1) }}"   >View More <i class="ecicon eci-chevron-right"></i></a>
+@endif
 
