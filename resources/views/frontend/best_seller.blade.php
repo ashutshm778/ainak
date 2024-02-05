367bd16b0181f06a710d4a46506d604b07d22bae
ashutshm778 <ashutoshm778@gmail.com>
2024-02-05 14:15:14 +0530

    new update

--- resources/views/frontend/best_seller.blade.php
+++ resources/views/frontend/best_seller.blade.php
@@ -5,7 +5,4 @@
 @endphp
 @include('frontend.product')
 @endforeach
-@if($best_sellers->currentPage()==$best_sellers->lastPage())
-@else
 <a style="display:none;" id="current_page_best_sellers" name="example2" class="btn btn-secondary rds {{ ($best_sellers->currentPage() == $best_sellers->lastPage()) ? ' disabled' : '' }}" href="{{ $best_sellers->url($best_sellers->currentPage()+1) }}"   >View More <i class="ecicon eci-chevron-right"></i></a>
-@endif
