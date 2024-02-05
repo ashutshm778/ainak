1799be8b0c8c8c31824df3b28b4d1fbd4530f8d2
ashutshm778 <ashutoshm778@gmail.com>
2024-02-05 14:41:36 +0530

    new update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -809,8 +809,8 @@
         function featured_product(){
             event.preventDefault();
             var page = $("a[name='example1']:last").attr('href').split('page=')[1];
-            var current_page_featured = $("input[name='current_page_featured']:last").val();
-            var last_page_featured = $("input[name='last_page_featured']:last").val();
+            var current_page_featured = $("input[name='current_page_featureds']:last").val();
+            var last_page_featured = $("input[name='last_page_featureds']:last").val();
             if(current_page_featured!=last_page_featured){
                 feature(page);
             }
